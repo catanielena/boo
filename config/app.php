b8422cc7b966eba698c79af0847fc973fff05796
catanielena <catani.elena1@gmail.com>
2022-01-05 15:13:30 +0100

    add: db config

--- config/app.php
+++ config/app.php
@@ -54,8 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    // 'asset_url' => env('ASSET_URL', null),
-    env('ASSET_URL', 'http://delivebooo.herokuapp.com/'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
